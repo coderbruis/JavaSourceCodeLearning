59ee2dc8f6f5095d02cc1d4c7dd35b6f9d3201a7
haiyang.luo <luohaiyangvs@qq.com>
2021-02-26 12:08:23 +0800

    [A] 添加Java零拷贝实现case

--- JdkLearn/src/main/java/com/learnjava/io/nio/MappedByteBufferTest.java
+++ JdkLearn/src/main/java/com/learnjava/io/nio/MappedByteBufferTest.java
@@ -1,5 +1,8 @@
 package com.learnjava.io.nio;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
@@ -17,13 +20,55 @@ public class MappedByteBufferTest {
 
         MappedByteBuffer mappedByteBuffer = channel.map(FileChannel.MapMode.READ_WRITE, 0, 5);
 
-        mappedByteBuffer.put(0, (byte)'H');
-        mappedByteBuffer.put(3, (byte)'9');
+        mappedByteBuffer.put(0, (byte) 'H');
+        mappedByteBuffer.put(3, (byte) '9');
 
         // IndexOutOfBoundsException
-        mappedByteBuffer.put(5, (byte)'Y');
+        mappedByteBuffer.put(5, (byte) 'Y');
 
         randomAccessFile.close();
         System.out.println("change success");
+    }
+
+    /**
+     *
+     * @param from
+     * @param to
+     * @throws IOException
+     */
+    public static void mmap4zeroCopy(String from, String to) throws IOException {
+        FileChannel source = null;
+        FileChannel destination = null;
+        try {
+            source = new RandomAccessFile(from, "r").getChannel();
+            destination = new RandomAccessFile(to, "rw").getChannel();
+            MappedByteBuffer inMappedBuf =
+                    source.map(FileChannel.MapMode.READ_ONLY, 0, source.size());
+            destination.write(inMappedBuf);
+        } finally {
+            if (source != null) {
+                source.close();
+            }
+            if (destination != null) {
+                destination.close();
+            }
+        }
+    }
+
+    public static void sendfile4zeroCopy(String from, String to) throws IOException{
+        FileChannel source = null;
+        FileChannel destination = null;
+        try {
+            source = new FileInputStream(from).getChannel();
+            destination = new FileOutputStream(to).getChannel();
+            source.transferTo(0, source.size(), destination);
+        } finally {
+            if (source != null) {
+                source.close();
+            }
+            if (destination != null) {
+                destination.close();
+            }
+        }
     }
 }
