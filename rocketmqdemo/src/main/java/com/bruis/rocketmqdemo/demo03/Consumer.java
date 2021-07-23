6f17e5c23093d24bc7c4b4ee0c43cbcea2191563
Bruis <luohaiyangvs@qq.com>
2021-07-23 23:34:04 +0800

    添加顺序消费demo

---
+++ rocketmqdemo/src/main/java/com/bruis/rocketmqdemo/demo03/Consumer.java
@@ -0,0 +1,61 @@
+package com.bruis.rocketmqdemo.demo03;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerOrderly;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 顺序消息消费，带事务方式（应用可控制Offset什么时候提交）
+ *
+ * @author lhy
+ * @date 2021/7/23
+ */
+public class Consumer {
+
+    public static void main(String[] args) throws Exception {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(Producer.GROUP_NAME);
+        consumer.setNamesrvAddr(Producer.NAMESRV_ADDRESS);
+
+        /**
+         * 设置Consumer第一次启动是从队列头部开始消费还是队列尾部开始消费
+         * 如果非第一次启动，那么按照上次消费的位置继续消费
+         */
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+        consumer.subscribe(Producer.TOPIC_NAME, "TagA || TagC || TagD");
+
+        /**
+         * 顺序消费的消息类？？MessageListenerOrderly
+         */
+        consumer.registerMessageListener(new MessageListenerOrderly() {
+
+            Random random = new Random();
+
+            @Override
+            public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext context) {
+                context.setAutoCommit(true);
+                for (MessageExt msg : msgs) {
+                    // 可以看到每个queue有唯一的consume线程来消费, 订单对每个queue(分区)有序
+                    System.out.println("consumeThread=" + Thread.currentThread().getName() + "queueId=" + msg.getQueueId() + ", content:" + new String(msg.getBody()));
+                }
+
+                try {
+                    //模拟业务逻辑处理中...
+                    TimeUnit.SECONDS.sleep(random.nextInt(10));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return ConsumeOrderlyStatus.SUCCESS;
+            }
+        });
+
+        consumer.start();
+        System.out.println("Consumer Started.");
+    }
+}
---
+++ rocketmqdemo/src/main/java/com/bruis/rocketmqdemo/demo03/Producer.java
@@ -0,0 +1,151 @@
+package com.bruis.rocketmqdemo.demo03;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * 顺序消息样例
+ *
+ * @author lhy
+ * @date 2021/7/23
+ */
+public class Producer {
+
+    public static final String NAMESRV_ADDRESS = "127.0.0.1:9876";
+
+    public static final String GROUP_NAME = "inorder_group_name";
+
+    public static final String TOPIC_NAME = "inorder_topic";
+
+    public static void main(String[] args) throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer(GROUP_NAME);
+        producer.setNamesrvAddr(NAMESRV_ADDRESS);
+
+        producer.start();
+
+        String[] tags = {"TagA", "TagC", "TagD"};
+
+        // 订单列表
+        List<OrderStep> orderList = new Producer().buildOrders();
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateStr = sdf.format(date);
+        for (int i = 0; i < 10; i++) {
+            // 时间前缀
+            String body = dateStr + " Hello RocketMQ " + orderList.get(i);
+            Message msg = new Message(TOPIC_NAME, tags[i % tags.length], "KEY" + i, body.getBytes());
+
+            SendResult sendResult = producer.send(msg, (mqs, msg1, arg) -> {
+                Long id = (Long) arg;  //根据订单id选择发送queue
+                long index = id % mqs.size();
+                return mqs.get((int) index);
+            }, orderList.get(i).getOrderId());//订单id
+
+           System.out.println(String.format("SendResult status:%s, queueId:%d, body:%s",
+               sendResult.getSendStatus(),
+               sendResult.getMessageQueue().getQueueId(),
+               body));
+        }
+
+        producer.shutdown();
+    }
+
+    /**
+     * 订单的步骤
+     */
+    private class OrderStep {
+        private long orderId;
+        private String desc;
+
+        public long getOrderId() {
+            return orderId;
+        }
+
+        public void setOrderId(long orderId) {
+            this.orderId = orderId;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+
+        @Override
+        public String toString() {
+            return "OrderStep{" +
+                    "orderId=" + orderId +
+                    ", desc='" + desc + '\'' +
+                    '}';
+        }
+    }
+
+    /**
+     * 生成模拟订单数据
+     */
+    private List<OrderStep> buildOrders() {
+        List<OrderStep> orderList = new ArrayList<OrderStep>();
+
+        OrderStep orderDemo = new OrderStep();
+        orderDemo.setOrderId(15103111039L);
+        orderDemo.setDesc("创建");
+        orderList.add(orderDemo);
+
+        orderDemo = new OrderStep();
+        orderDemo.setOrderId(15103111065L);
+        orderDemo.setDesc("创建");
+        orderList.add(orderDemo);
+
+        orderDemo = new OrderStep();
+        orderDemo.setOrderId(15103111039L);
+        orderDemo.setDesc("付款");
+        orderList.add(orderDemo);
+
+        orderDemo = new OrderStep();
+        orderDemo.setOrderId(15103117235L);
+        orderDemo.setDesc("创建");
+        orderList.add(orderDemo);
+
+        orderDemo = new OrderStep();
+        orderDemo.setOrderId(15103111065L);
+        orderDemo.setDesc("付款");
+        orderList.add(orderDemo);
+
+        orderDemo = new OrderStep();
+        orderDemo.setOrderId(15103117235L);
+        orderDemo.setDesc("付款");
+        orderList.add(orderDemo);
+
+        orderDemo = new OrderStep();
+        orderDemo.setOrderId(15103111065L);
+        orderDemo.setDesc("完成");
+        orderList.add(orderDemo);
+
+        orderDemo = new OrderStep();
+        orderDemo.setOrderId(15103111039L);
+        orderDemo.setDesc("推送");
+        orderList.add(orderDemo);
+
+        orderDemo = new OrderStep();
+        orderDemo.setOrderId(15103117235L);
+        orderDemo.setDesc("完成");
+        orderList.add(orderDemo);
+
+        orderDemo = new OrderStep();
+        orderDemo.setOrderId(15103111039L);
+        orderDemo.setDesc("完成");
+        orderList.add(orderDemo);
+
+        return orderList;
+    }
+
+}
